f6b84892f27f366764954bf4e7b499cc98acacdb
kihabiba91-maker <kihabiba91@gmail.com>
2026-01-09 09:41:30 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App.tsx';
+import App from './src/components/App'
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
