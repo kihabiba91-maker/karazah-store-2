a373468988fc9e26770f626dbe98871102a28e57
kihabiba91-maker <kihabiba91@gmail.com>
2026-01-09 06:05:16 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -8,17 +8,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
     emptyOutDir: true,
     rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    host: true
+      input: {
+        main: './index.html',
+      },
+    },
   }
 });
